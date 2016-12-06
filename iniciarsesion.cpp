9bd903b9bf1544fbb4f740ae40f9aacdfa80c97a
jorjoluiso@gmail.com <jorjoluiso@gmail.com>
2016-12-05 20:20:02 -0500

    Añadido iniciar sesión

---
+++ iniciarsesion.cpp
@@ -0,0 +1,14 @@
+#include "iniciarsesion.h"
+#include "ui_iniciarsesion.h"
+
+IniciarSesion::IniciarSesion(QWidget *parent) :
+    QDialog(parent),
+    ui(new Ui::IniciarSesion)
+{
+    ui->setupUi(this);
+}
+
+IniciarSesion::~IniciarSesion()
+{
+    delete ui;
+}
---
+++ iniciarsesion.h
@@ -0,0 +1,22 @@
+#ifndef INICIARSESION_H
+#define INICIARSESION_H
+
+#include <QDialog>
+
+namespace Ui {
+class IniciarSesion;
+}
+
+class IniciarSesion : public QDialog
+{
+    Q_OBJECT
+
+public:
+    explicit IniciarSesion(QWidget *parent = 0);
+    ~IniciarSesion();
+
+private:
+    Ui::IniciarSesion *ui;
+};
+
+#endif // INICIARSESION_H
